d2c9dc8d7b55c482d4ac642a08f0e3f26848ac13
LonelyWolf <105735165+lonelyWolf-developer@users.noreply.github.com>
2023-09-06 18:36:16 +0200

    Create base interface and Numseries.cs interface.

---
+++ WolfsGames.Data/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WolfsGames.Data.Interfaces
+{
+	public interface IRepository<TEntity>
+	{
+		List<TEntity> GetAll();
+		TEntity FindById(int id);
+		TEntity FindByUrl(string url);
+		void Insert(TEntity entity);
+		void Update(TEntity entity);
+		void Delete(int id);
+	}
+}
